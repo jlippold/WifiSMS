0ea7b5839d750232f1591c0fe33f9dd1204592ff
Filipe Pina <fopina@skmobi.com>
2012-03-15 09:37:45 +0000

    binary files under version control? nono. binary files under version control AND duplicated? nononono.

--- wifiSMS/build/WifiSMS.build/Release-iphoneos/WifiSMS.build/Objects-normal/armv7/DDRange.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/TreAsoN/WifiSMS/wifiSMS/DDRange.m \
-  /Users/TreAsoN/WifiSMS/wifiSMS/DDRange.h \
-  /Users/TreAsoN/WifiSMS/wifiSMS/DDNumber.h
--- wifiSMS/build/WifiSMS.build/Release-iphoneos/WifiSMS.build/Objects-normal/armv7/wifiSMSDelegate.d
+++
@@ -1,5 +0,0 @@
-dependencies: /Users/TreAsoN/WifiSMS/wifiSMS/Classes/wifiSMSDelegate.m \
-  /Users/TreAsoN/WifiSMS/wifiSMS/Classes/wifiSMSDelegate.h \
-  /Users/TreAsoN/WifiSMS/wifiSMS/HTTPServer.h \
-  /Users/TreAsoN/WifiSMS/wifiSMS/Classes/MyHTTPConnection.h \
-  /Users/TreAsoN/WifiSMS/wifiSMS/HTTPConnection.h
